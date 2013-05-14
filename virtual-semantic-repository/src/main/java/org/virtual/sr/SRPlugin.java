24932cbf0af1413d5be88ab0ae24288697b149e4
fabiosimeoni <fabio.simeoni@me.com>
2013-05-14 17:13:37 +0200

    placeholder for plugin interface and tests

---
+++ virtual-semantic-repository/src/main/java/org/virtual/sr/SRPlugin.java
@@ -0,0 +1,16 @@
+package org.virtual.sr;
+
+import java.util.Collection;
+
+import org.virtualrepository.RepositoryService;
+import org.virtualrepository.spi.Plugin;
+
+public class SRPlugin implements Plugin {
+
+	@Override
+	public Collection<RepositoryService> services() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ virtual-semantic-repository/src/test/java/org/acme/SrTest.java
@@ -0,0 +1,13 @@
+package org.acme;
+
+import org.junit.Test;
+
+
+public class SrTest {
+
+	
+	@Test
+	public void test() {
+		
+	}
+}
