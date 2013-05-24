5e18bf506a39326a1473b430a6b6eb2b86213a9f
cbaldassarre <claudio.baldax@gmail.com>
2013-05-24 16:31:44 +0200

    inputAPI has now the Model instead of ResultSet type returned

--- virtual-semantic-repository/src/main/java/org/virtual/sr/transforms/codelist/Rdf2SdmxCodelist.java
+++ virtual-semantic-repository/src/main/java/org/virtual/sr/transforms/codelist/Rdf2SdmxCodelist.java
@@ -16,67 +16,84 @@ import org.virtualrepository.RepositoryService;
 import org.virtualrepository.sdmx.SdmxCodelist;
 import org.virtualrepository.spi.Transform;
 
-import com.hp.hpl.jena.query.QuerySolution;
-import com.hp.hpl.jena.query.ResultSet;
+import com.hp.hpl.jena.rdf.model.Model;
+import com.hp.hpl.jena.rdf.model.ResIterator;
+import com.hp.hpl.jena.rdf.model.Resource;
+import com.hp.hpl.jena.rdf.model.Statement;
+import com.hp.hpl.jena.rdf.model.StmtIterator;
+import com.hp.hpl.jena.vocabulary.RDF;
+import com.hp.hpl.jena.vocabulary.RDFS;
 
 /**
- * A {@link Transform}s from arbitrary {@link Asset}s in given APIs to RDF.
- * <p>
- * Bridges {@link XmlTransform}s with the {@link Xml2Rdf} transform to adapt the {@link RepositoryService} to given asset types.
- * 
+ * A {@link Transform}s from arbitrary {@link Asset}s in given APIs to RDF. <p>
+ * Bridges {@link XmlTransform}s with the {@link Xml2Rdf} transform to adapt the {@link RepositoryService}
+ * to given asset types.
+ *
  * @author Fabio Simeoni
  *
  */
-public class Rdf2SdmxCodelist implements Transform<SdmxCodelist,ResultSet,CodelistBean> {
+public class Rdf2SdmxCodelist implements Transform<SdmxCodelist, Model, CodelistBean> {
 
-	private static Logger log = LoggerFactory.getLogger(Rdf2SdmxCodelist.class);
-	
-	@Override
-	public CodelistBean apply(SdmxCodelist asset, ResultSet results) throws Exception {
-		
-		log.info("transforming codelist "+asset.id()+" to sdmx");
+    private static Logger log = LoggerFactory.getLogger(Rdf2SdmxCodelist.class);
 
-		CodelistMutableBean codelist = new CodelistMutableBeanImpl();
-		
-		Properties props = asset.properties();
-		
-		if (props.contains(Constants.ownerName))
-			codelist.setAgencyId(props.lookup(Constants.ownerName).value(String.class));
-		else
-			codelist.setAgencyId("SDMX");
-		
-		codelist.setId(asset.name());
-		codelist.setUri(asset.id());
-		codelist.addName("en",asset.name());
-		
-		while (results.hasNext()) {
+    @Override
+    public CodelistBean apply(SdmxCodelist asset, Model m) throws Exception {
+
+        log.info("transforming codelist " + asset.id() + " to sdmx");
+
+        CodelistMutableBean codelist = new CodelistMutableBeanImpl();
+
+        Properties props = asset.properties();
+
+        if (props.contains(Constants.ownerName)) {
+            codelist.setAgencyId(props.lookup(Constants.ownerName).value(String.class));
+        } else {
+            codelist.setAgencyId("SDMX");
+        }
+
+        codelist.setId(asset.name());
+        codelist.setUri(asset.id());
+        codelist.addName("en", asset.name());
+
+        ResIterator codes = m.listSubjects();
+
+        while (codes.hasNext()) {
+            Resource code_resource = codes.next();
             
-			QuerySolution next = results.next();
             CodeMutableBean code = new CodeMutableBeanImpl();
+            code.setId(code_resource.getPropertyResourceValue(RDF.value).asLiteral().getLexicalForm().replace(".", "_"));
+            code.setUri(code_resource.getURI());
+            StmtIterator names = code_resource.listProperties(RDFS.label);
+            while (names.hasNext()) {
+                Statement name_lit = names.next();
+                //TODO: replace with for loop over known bindings
+                if (!name_lit.getLiteral().getLexicalForm().isEmpty()) {
+                    code.addName(name_lit.getLanguage(), name_lit.getLiteral().getLexicalForm());
+                }
+            }
             
-            code.setId(next.getLiteral("code_lit").getLexicalForm().replace(".", "_"));
-            code.setUri(next.getResource("code").getURI());
-            
-            //TODO: replace with for loop over known bindings
-            if (!next.getLiteral("en_name").getLexicalForm().isEmpty())
-            	code.addName("en",next.getLiteral("en_name").getLexicalForm());
-            
-            //TODO: pull all comments and map them onto SDMX description
-            //code.addDescription(locale, name)
-            
+            StmtIterator descriptions = code_resource.listProperties(RDFS.comment);
+            while (descriptions.hasNext()) {
+                Statement desc_lit = descriptions.next();
+                //TODO: replace with for loop over known bindings
+                if (!desc_lit.getLiteral().getLexicalForm().isEmpty()) {
+                    code.ddDescription("en", desc_lit.getLiteral().getLexicalForm());
+                }
+            }
+
             codelist.addItem(code);
-        } 
-		
-		return codelist.getImmutableInstance(); 
-	}
+        }
 
-	@Override
-	public Class<ResultSet> inputAPI() {
-		return ResultSet.class;
-	}
+        return codelist.getImmutableInstance();
+    }
 
-	@Override
-	public Class<CodelistBean> outputAPI() {
-		return CodelistBean.class;
-	}
+    @Override
+    public Class<Model> inputAPI() {
+        return Model.class;
+    }
+
+    @Override
+    public Class<CodelistBean> outputAPI() {
+        return CodelistBean.class;
+    }
 }
