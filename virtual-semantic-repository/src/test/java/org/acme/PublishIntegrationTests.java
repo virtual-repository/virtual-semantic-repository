ac19bc844d6f866948c5852a8ff2ab9418645d15
fabiosimeoni <fabio.simeoni@me.com>
2014-04-07 16:52:45 +0200

    moved to 1.1.0 with test dep on virtual-rtms-2.0.0

--- virtual-semantic-repository/src/test/java/org/acme/PublishIntegrationTests.java
+++ virtual-semantic-repository/src/test/java/org/acme/PublishIntegrationTests.java
@@ -3,14 +3,19 @@ package org.acme;
 import static org.sdmx.SdmxServiceFactory.*;
 
 import java.io.InputStream;
+import java.util.Iterator;
 
+import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.sdmx.SdmxServiceFactory;
 import org.sdmxsource.sdmx.api.model.beans.codelist.CodelistBean;
 import org.sdmxsource.util.io.ReadableDataLocationTmp;
 import org.virtual.sr.RepositoryPlugin;
+import org.virtualrepository.Asset;
 import org.virtualrepository.RepositoryService;
 import org.virtualrepository.VirtualRepository;
+import org.virtualrepository.csv.CsvCodelist;
 import org.virtualrepository.impl.Repository;
 import org.virtualrepository.sdmx.SdmxCodelist;
 
@@ -40,5 +45,38 @@ public class PublishIntegrationTests {
 		repo.publish(asset,list);
                 
 		
+	}
+	
+	@Test
+	public void another() {
+		
+		SdmxServiceFactory.init();
+		
+		//start vr
+		VirtualRepository repo = new Repository();
+		
+		//discover codelists in sdmx
+		repo.discover(SdmxCodelist.type);
+
+		//get first discovered codelist
+		Iterator<Asset> it = repo.iterator();
+		
+		it.next();
+		
+		Asset discoveredAsset = it.next();
+		
+		//retrieve its content
+		CodelistBean sdmx = repo.retrieve(discoveredAsset,CodelistBean.class);
+		
+		//get virtual semantic repo 
+		RepositoryService service = repo.services().lookup(RepositoryPlugin.name);
+		
+		//create asset for publication
+		SdmxCodelist publishAsset = new SdmxCodelist("test", service);
+		
+		//public sdmx with virtual repo
+		repo.publish(publishAsset,sdmx);
+		
+	
 	}
 }
