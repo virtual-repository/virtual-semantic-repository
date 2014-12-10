1d3b7c180f3b5054ce3329a8bcb7088c7d6933d8
fabiosimeoni <fabio.simeoni@me.com>
2014-12-10 22:16:44 +0100

    now with dynamic xml for csv conversion

--- virtual-semantic-repository/src/main/java/org/virtual/sr/transforms/Csv2Xml.java
+++ virtual-semantic-repository/src/main/java/org/virtual/sr/transforms/Csv2Xml.java
@@ -1,15 +1,11 @@
 package org.virtual.sr.transforms;
 
+import static java.lang.String.*;
+
 import java.io.StringReader;
-import java.io.StringWriter;
-import java.util.ArrayList;
 import java.util.List;
 
-import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
-import javax.xml.bind.annotation.XmlAttribute;
-import javax.xml.bind.annotation.XmlElement;
-import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.transform.Source;
 import javax.xml.transform.stream.StreamSource;
 
@@ -20,73 +16,31 @@ import org.virtualrepository.tabular.Table;
 
 public class Csv2Xml implements XmlTransform<Table> {
 	
-	@XmlRootElement(name="table")
-	static class XmlTable {
-		
-		@XmlElement(name="row")
-		List<XmlRow> rows = new ArrayList<XmlRow>();
-		
-	}
-	
-	static class XmlRow {
-		
-		@XmlElement(name="cell")
-		List<XmlCell> cells = new ArrayList<XmlCell>();
-		
-	}
-	
-	
-	static class XmlCell {
-		
-		public XmlCell(String col, String val) {
-			this.col=col;
-			this.val=val;
-		}
-		@XmlAttribute
-		String col;
-		
-		@XmlAttribute
-		String val;
-		
-	}
-	
-	private JAXBContext _ctx;
-
-	public Csv2Xml() {
-		
-		try {
-			this._ctx = JAXBContext.newInstance(XmlTable.class);
-		} catch (JAXBException ex) {
-			throw new RuntimeException("Failed JAXB initialization", ex);
-		}
-	}
-
 	@Override
 	public Source toXml(Table table, Asset asset) throws JAXBException {
 		
-		XmlTable xtable = new XmlTable();
 		
+		String xtable = "";
 		//write
 		List<Column> cols = table.columns();
 		
 		for (Row row : table) {
 			
-			XmlRow xrow= new XmlRow();
+			String cells = "";
 			
 			for  (Column col : cols) {
 				String val =  row.get(col);
 				if (val!=null)
-					xrow.cells.add(new XmlCell(col.name().toString(),val));
+					cells = cells+ format("<%1$s>%2$s</%1$s>",col.name().toString(),val);
 			}
 			
-			xtable.rows.add(xrow);
+			xtable = xtable + format("<row>%s</row>",cells);
 		}
 		
-		StringWriter sw = new StringWriter();
-
-		this._ctx.createMarshaller().marshal(xtable, sw);
 		
-		return new StreamSource(new StringReader(sw.toString()));
+		String xml = format("<?xml version='1.0'?><table>%s</table>",xtable);
+		
+		return new StreamSource(new StringReader(xml));
 	}
 
 	@Override
